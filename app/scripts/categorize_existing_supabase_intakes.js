ee8493cad279b682e44cff0c23a92b4c745d3a35
Pablo Hernando Marrugat <pablo@MacBook-Air-de-Pablo.local>
2026-08-08 09:05:47 +0800

    fix(categories): prioritize desserts, ice cream, and beverages over fruit keywords with word boundary matching

--- app/scripts/categorize_existing_supabase_intakes.js
+++ app/scripts/categorize_existing_supabase_intakes.js
@@ -28,38 +28,49 @@ const supabase = createClient(supabaseUrl, supabaseAnonKey);
 function classifyFoodName(name) {
   const n = (name || '').toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 
-  if (/donut|croissant|pandesal|magdalena|bizcocho|galleta|tarta|pastel|bollo|bolleria|waffle|crepe|muffin|brownie|pancakes|pancake/i.test(n)) {
-    return 'bakery';
-  }
-  if (/patata|papa|boniato|camote|yuca|tuberculo|sweet.*potato|potato/i.test(n)) {
-    return 'tubers';
-  }
-  if (/pizza|burger|mcdonald|kfc|kebab|burrito|croqueta|churro|empanada|hot.*dog|doritos|snack/i.test(n)) {
+  // 1. Fast Food
+  if (/\b(pizza|burger|hamburguesa|mcdonald|kfc|kebab|burrito|croqueta|churro|empanada|hotdog|hot.*dog|doritos|cheetos|snack|chips)\b/i.test(n) || /patata.*frita/i.test(n)) {
     return 'fast_food';
   }
-  if (/pollo|pavo|ternera|cerdo|carne|lomo|entrecot|jamon|bacon|pescado|atun|salmon|merluza|huevo|tortilla|clara|gamba|calamar|pulpo|bacalao|solomillo|chuleton|yema/i.test(n)) {
+  // 2. Bakery & Desserts (takes precedence over fruits if it's ice cream, cake, sweets, etc.)
+  if (/\b(helado|tarta|pastel|pasteles|postre|postres|donut|donuts|croissant|pandesal|magdalena|bizcocho|galleta|galletas|bollo|bolleria|waffle|crepe|muffin|brownie|pancake|pancakes|flan|natillas|chocolate|chocolates|candy|ice.*cream|tiramisu)\b/i.test(n) || /mango.*graham|ice.*candy/i.test(n)) {
+    return 'bakery';
+  }
+  // 3. Beverages (takes precedence over fruits if it's juice, smoothie, shake)
+  if (/\b(cafe|infusion|agua|zumo|jugo|batido|batidos|proteina|whey|cerveza|vino|refresco|coca.*cola|fanta|sprite|pepsi|smoothie|colacao|nesquik|leche.*chocolate)\b/i.test(n) || /\bte\b/i.test(n)) {
+    return 'beverages';
+  }
+  // 4. Meat & Fish
+  if (/\b(pollo|pavo|ternera|cerdo|carne|lomo|entrecot|jamon|bacon|pescado|atun|salmon|merluza|huevo|huevos|tortilla|clara|claras|gamba|gambas|calamar|pulpo|bacalao|solomillo|chuleton|yema|yemas|pork|beef|chicken|steak|fish)\b/i.test(n) || /omelette/i.test(n)) {
     return 'meat';
   }
-  if (/lenteja|garbanzo|alubia|judia|frijol|soja|edamame|tofu|hummus/i.test(n)) {
+  // 5. Tubers & Potatoes
+  if (/\b(patata|patatas|papa|papas|boniato|boniatos|camote|yuca|tuberculo|batata|batatas|potato|potatoes|sweet.*potato)\b/i.test(n)) {
+    return 'tubers';
+  }
+  // 6. Legumes
+  if (/\b(lenteja|lentejas|garbanzo|garbanzos|alubia|alubias|judia|judias|frijol|frijoles|soja|edamame|tofu|hummus|lentils)\b/i.test(n)) {
     return 'legumes';
   }
-  if (/ensalada|lechuga|tomate|pepino|cebolla|zanahoria|espinaca|brocoli|coliflor|calabacin|pimiento|champinon|seta|verdura|canonigo|rucula|esparrago|ajo/i.test(n)) {
+  // 7. Vegetables
+  if (/\b(ensalada|lechuga|tomate|tomates|pepino|cebolla|zanahoria|espinaca|espinacas|brocoli|coliflor|calabacin|pimiento|pimientos|champinon|champinones|seta|setas|verdura|verduras|canonigo|rucula|esparrago|esparragos|ajo)\b/i.test(n)) {
     return 'vegetables';
   }
-  if (/manzana|platano|banana|fresa|naranja|mandarina|uva|melon|sandia|pina|kiwi|melocoton|mango|fruta|arandano|frambuesa/i.test(n)) {
+  // 8. Raw Fruits
+  if (/\b(manzana|manzanas|platano|platanos|banana|bananas|fresa|fresas|naranja|naranjas|mandarina|mandarinas|uva|uvas|melon|sandia|pina|kiwi|melocoton|mango|fruta|frutas|arandano|arandanos|frambuesa|ciruela|cereza|apple|orange)\b/i.test(n)) {
     return 'fruit';
   }
-  if (/leche|yogur|yogurt|queso|cuajada|kefir|mantequilla|nata|requeson/i.test(n)) {
+  // 9. Dairy
+  if (/\b(leche|yogur|yogurt|queso|quesos|cuajada|kefir|mantequilla|nata|requeson|milk|cheese|yogurt)\b/i.test(n)) {
     return 'dairy';
   }
-  if (/arroz|pan|pasta|espagueti|macarron|avena|cereal|harina|quinoa|tostada|noodle|ramen/i.test(n)) {
+  // 10. Grains
+  if (/\b(arroz|pasta|espagueti|macarron|macarrones|avena|cereal|cereales|harina|quinoa|tostada|tostadas|noodle|noodles|ramen|rice|bread|oats)\b/i.test(n) || /\bpan\b/i.test(n)) {
     return 'grains';
   }
-  if (/aceite|oliva|fruto.*seco|nuez|almendra|avellana|pistacho|aguacate|cacahuete|crema.*mani/i.test(n)) {
+  // 11. Healthy Fats
+  if (/\b(aceite|oliva|nuez|nueces|almendra|almendras|avellana|avellanas|pistacho|pistachos|aguacate|cacahuete|cacahuetes)\b/i.test(n) || /fruto.*seco|crema.*mani/i.test(n)) {
     return 'healthy_fats';
-  }
-  if (/cafe|te|infusion|agua|zumo|jugo|batido|proteina|whey|cerveza|vino|refresco|coca.*cola/i.test(n)) {
-    return 'beverages';
   }
 
   return 'other';
